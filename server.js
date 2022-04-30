8d75b87ff45cbd86766e72b229630cd87be8809b
Jacob Terrell <jacobit97@hotmail.com>
2022-04-30 01:15:42 -0600

    proccess env

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const getDocument = require('./routes/contacts');
 const app = express();
 const port = process.env.PORT || 3000;
 
-connectDB();
-app.use(express.json({extensions: false}));
-app.use('/', require('./routes/index'))
+
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
 })
+
+connectDB();
+app.use(express.json({extensions: false}));
+app.use('/', require('./routes/index'))
