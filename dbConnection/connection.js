90705fcf001a7cf0a549bea2f6a0ee233ffa59a8
Jacob Terrell <jacobit97@hotmail.com>
2022-04-30 00:59:50 -0600

    maybe updated

--- dbConnection/connection.js
+++ dbConnection/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
 const connectDB = async () => {
     mongoose.connect(process.env.mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
     console.log('db connected');
